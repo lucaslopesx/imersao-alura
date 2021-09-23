a3e73bec475735c813df57bce4b0a91e1d5884c5
Lucas Lopes <68665689+lucaslopesx@users.noreply.github.com>
2021-09-23 16:14:59 -0300

    commit aula07-supertrunfo OK

--- Aula06/script.js
+++ Aula06/script.js
@@ -29,8 +29,8 @@ function exibeJogadores(jogadores) {
         elemento += `<td>${jogadores[i].derrotas}</td>`;
         elemento += `<td>${jogadores[i].pontos}</td>`;
         elemento += `<td><button onClick="adicionarVitoria(${i})">Vitória</button></td>`;
-        elemento += `<td><button onClick=>Empate</button></td>`;
-        elemento += `<td><button onClick=>Derrota</button></td>`;
+        elemento += `<td><button onClick="adicionarEmpate(${i})">Empate</button></td>`;
+        elemento += `<td><button onClick="adicionarDerrota(${i})">Derrota</button></td>`;
         elemento += `</tr>`;
     }
     let tabelaJogadores = document.getElementById("tabelaJogadores");
@@ -40,6 +40,20 @@ function exibeJogadores(jogadores) {
 function adicionarVitoria(i) {
     let jogador = jogadores[i];
     jogador.vitorias++;
+    jogador.pontos = calculaPontos(jogador);
+    exibeJogadores(jogadores);
+}
+
+function adicionarEmpate(i) {
+    let jogador = jogadores[i];
+    jogador.empates++;
+    jogador.pontos = calculaPontos(jogador);
+    exibeJogadores(jogadores);
+}
+
+function adicionarDerrota(i) {
+    let jogador = jogadores[i];
+    jogador.derrotas++;
     jogador.pontos = calculaPontos(jogador);
     exibeJogadores(jogadores);
 }
